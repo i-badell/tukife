41077142094845e60faef1b68778718960312eb3
ibadell <ibadell@virtuoso.com>
2025-06-28 12:37:22 -0300

    Spinner bg

--- src/components/spinner.vue
+++ src/components/spinner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="loading" class="absolute inset-0 bg-white/70 flex items-center justify-center z-50">
+  <div v-if="loading" class="absolute inset-0 bg-white flex items-center justify-center z-50">
     <div class="w-10 h-10 border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
   </div>
 </template>
